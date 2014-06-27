64e42c10c7e1b02302c0787088cdf83474146519
shiva <shivraj.s@hashedin.com>
2014-06-27 17:29:11 +0530

    Done with project lists page working on project details page

--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -5,7 +5,98 @@
 angular.module('huPortalApp.controllers', [])
   .controller('projectListController', ['$scope', function($scope) {
      $scope.message = "Hello This message is from Project List";
+
+     $scope.projects = [
+
+			{			
+				"projectId" : "1",
+				"projectName" : "Responsive Web",
+				"description" : "done using boot strap",
+				"coordinators" : ["shiva","sampath","sayeesh","ravi","masroor"]
+
+			},
+
+			{
+				"projectId" : "2",
+				"projectName" : "Angular JS",
+				"description" : "done using angular js ",
+				"coordinators" : ["shiva","sampath","sayeesh","ravi","masroor"]
+
+			}
+
+     	];
   }])
-  .controller('projectDetailsController', ['$scope', function($scope) {
+  .controller('projectDetailsController', ['$scope', '$routeParams', function($scope) {
+
+  	// var projId = $routeParams.projectId;
+
     $scope.message = "Hello This message is from Project Details";
+
+    $scope.projects = [
+
+    		{
+    			"projectId" : "1",
+				"projectName" : "Responsive Web",
+				"description" : "done using boot strap",
+				"coordinators" : ["shiva","sampath","sayeesh","ravi","masroor"],
+				"tasks" : 	[
+								{
+									"taskName" : "Create index page",
+									"taskDescription" : "Create a home page for the project",
+									"priority" : "0",
+									"assignedTo" : "shiva",
+									"dueDate" : 	"29/06/2014",
+									"createdOn" : "27/06/2014"
+								},
+
+								{
+									"taskName" : "Improve UI & UX",
+									"taskDescription" : "Create more pleasing UI",
+									"priority" : "2",
+									"assignedTo" : "sampath",
+									"dueDate" : "01/07/2014",
+									"createdOn" : "27/06/2014"
+								}
+							]
+
+    		},
+    // 		{
+    // 			"projectId" : "2",
+				// "projectName" : "Angular JS",
+				// "description" : "done using Angular js",
+				// "coordinators" : ["shiva","sampath","sayeesh","ravi","masroor"],
+				// "tasks" : 	[
+				// 				{
+				// 					"taskName" : "Create controllers",
+				// 					"taskDescription" : "Create a home page for the project",
+				// 					"priority" : "1",
+				// 					"assignedTo" : "shiva",
+				// 					"dueDate" : 	"29/06/2014",
+				// 					"createdOn" : "27/06/2014"
+				// 				},
+
+				// 				{
+				// 					"taskName" : "Improve UI & UX",
+				// 					"taskDescription" : "Create more pleasing UI",
+				// 					"priority" : "3",
+				// 					"assignedTo" : "sampath",
+				// 					"dueDate" : "01/07/2014",
+				// 					"createdOn" : "27/06/2014"
+				// 				}
+				// 			]
+
+    // 		}
+
+    	];
+
+
+    //finding the json by the id which i got from the url
+    // for(var i=0 ; i < $scope.projects.length; ++i)
+    // {
+    // 	if($scope.projects[i].projectId === $routeParams.projectId)
+    // 	{
+    // 		$scope.projectDetails = $scope.project[i];
+    // 	}
+    // }
+
   }]);
